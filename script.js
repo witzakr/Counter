a1dce5175304400dce7f066cda9fc307c031ffd4
I524303 <I524303@fhict.nl>
2024-02-28 12:43:42 +0100

    translations

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     var counterElement = document.querySelector('h3');
-    
+    var counterTextElement = document.querySelector('p');
+
     var counterValue = 0;
+    var translations = [];
+    var translationIndex = 0;
+
+    function fetchTranslations() {
+        fetch('https://mocki.io/v1/f75f3f33-672f-4b7e-8fdf-5e75019c52e9')
+            .then(response => response.json())
+            .then(data => {
+                translations = data.translations.map(entry => entry.translation);
+            })
+            .catch(error => {
+                console.error('Error fetching translations:', error);
+            });
+    }
 
     function updateCounter() {
         counterValue++;
@@ -10,12 +24,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
         var randomColour = '#' + Math.floor(Math.random()*16777215).toString(16);
         counterElement.style.color = randomColour;
+        counterTextElement.style.color = randomColour;
 
-        
         var randomBackgroundColour = '#' + Math.floor(Math.random()*16777215).toString(16);
         document.body.style.backgroundColor = randomBackgroundColour; 
 
+        counterTextElement.textContent = translations[translationIndex];
+        translationIndex = (translationIndex + 1) % translations.length;
     }
 
+    fetchTranslations();
+
+    setInterval(fetchTranslations, 1000);
+
     setInterval(updateCounter, 1000);
-});
+
+}); 
