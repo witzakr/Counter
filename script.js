828a3846af7495c40f357f781806d8362fe7404a
I524303 <I524303@fhict.nl>
2024-02-28 12:24:21 +0100

    making colours change

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     var counterElement = document.querySelector('h3');
+    
     var counterValue = 0;
 
     function updateCounter() {
         counterValue++;
         counterElement.textContent = counterValue;
 
-        var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        counterElement.style.color = randomColor;
+        var randomColour = '#' + Math.floor(Math.random()*16777215).toString(16);
+        counterElement.style.color = randomColour;
+
+        
+        var randomBackgroundColour = '#' + Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = randomBackgroundColour; 
 
     }
 
